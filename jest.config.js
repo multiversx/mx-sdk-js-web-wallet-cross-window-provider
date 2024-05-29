b4a290962da558cda8c027bdb9f7ebf424cbca97
Tudor Morar <tudor.morar@multiversx.com>
2024-05-29 14:45:20 +0300

    Update tests

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,5 @@ module.exports = {
   roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.[t|j]sx?$': 'ts-jest'
-  },
-  transformIgnorePatterns: ['node_modules/(^.+\\\\.(ts|js|tsx|jsx)$)']
+  }
 };
